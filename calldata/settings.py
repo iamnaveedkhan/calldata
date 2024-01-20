0301f912a9341e9809856b7ba89484be226d85fc
Naveed Khan <131237915+iamnaveedkhan@users.noreply.github.com>
2024-01-20 21:14:08 +0530

    Update settings.py

--- calldata/settings.py
+++ calldata/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,6 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+dotenv_file = os.path.join(BASE_DIR,".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-*r+x4u)uc)qu!s!tse&y@e&h+ns)8x^ubm)5-lk1w#q%sek1@j'
